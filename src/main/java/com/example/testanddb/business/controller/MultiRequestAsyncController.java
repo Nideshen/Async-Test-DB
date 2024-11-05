4bd2f3a3bddab3b880ddc7ed57984bf361c5eee8
潘王熙 <panwangxi2000@hotmail.com>
2024-11-05 15:40:13 +0800

    Multi Thread Async

--- src/main/java/com/example/testanddb/business/controller/MultiRequestAsyncController.java
+++ src/main/java/com/example/testanddb/business/controller/MultiRequestAsyncController.java
@@ -1,13 +1,10 @@
 package com.example.testanddb.business.controller;
 
 import com.example.testanddb.business.component.MultiRequestSharedState;
-import com.example.testanddb.business.component.SharedState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class MultiRequestAsyncController {
